bb51d7d3f57913aafcf33fabf189c5bcdad000eb
OurGay <106759405+OurGay@users.noreply.github.com>
2020-01-09 17:01:46 +0000

    Add travis ci support

--- engine.go
+++ engine.go
@@ -6,3 +6,26 @@ import (
 
 	"github.com/apex/log"
 )
+
+//Engine that runs strategies
+type Engine struct {
+	strategy *Strategy
+	om       *orderManagement
+
+	signals chan Signal
+
+	tradeCh  map[string]chan Trade
+	quoteCh  map[string]chan Quote
+	changeCh map[string]chan struct{}
+
+	ohlc   map[string]*History
+	quotes map[string]*Quote
+
+	quit chan struct{}
+}
+
+//Trade type
+type Trade struct {
+	Price, Amount float64
+	Time          time.Time
+}
