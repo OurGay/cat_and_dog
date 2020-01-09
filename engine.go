6f22729a5cf08d0e3ad3f842e1a6e231096ad479
OurGay <106759405+OurGay@users.noreply.github.com>
2020-01-09 22:53:07 +0000

    Fixed #36

--- engine.go
+++ engine.go
@@ -44,3 +44,21 @@ func NewEngine() *Engine {
 		changeCh: make(map[string]chan struct{}),
 		ohlc:     make(map[string]*History),
 		quit:     make(chan struct{}),
+		signals:  make(chan Signal),
+	}
+}
+
+//AddSymbol to engine - quotes and trades for this symbol
+func (e *Engine) AddSymbol(symbol string, quotes chan Quote, trades chan Trade) {
+	e.quoteCh[symbol] = quotes
+	e.tradeCh[symbol] = trades
+	e.changeCh[symbol] = make(chan struct{})
+
+	initTimeSeries(e, symbol)
+
+	e.quotes = make(map[string]*Quote)
+	e.quotes[symbol] = &Quote{}
+}
+
+//AddStrategy to engine
+func (e *Engine) AddStrategy(strategy *Strategy) {
