b88e9b1f0845f15c8f883a3ce1bd11f550c0b1b2
OurGay <106759405+OurGay@users.noreply.github.com>
2020-01-18 21:28:48 +0000

    Micro heart

--- engine_test.go
+++ engine_test.go
@@ -9,3 +9,22 @@ import (
 )
 
 var _ = Describe("Engine", func() {
+
+	Context("Symbol", func() {
+		It("Add", func() {
+			ngn := NewEngine()
+			quotes := make(chan Quote)
+			trades := make(chan Trade)
+			ngn.AddSymbol("EUR/USD", quotes, trades)
+		})
+
+		It("OHLC", func() {
+
+			symbol := "EUR/USD"
+
+			ngn := NewEngine()
+			quotes := make(chan Quote)
+			trades := make(chan Trade)
+			ngn.AddSymbol(symbol, quotes, trades)
+			ngn.Run()
+			defer ngn.Stop()
