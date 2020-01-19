85d63b5e4921d0bc971b3661bb65c24d6d0001d1
OurGay <106759405+OurGay@users.noreply.github.com>
2020-01-19 07:51:28 +0000

    fix log messages

--- engine_test.go
+++ engine_test.go
@@ -28,3 +28,19 @@ var _ = Describe("Engine", func() {
 			ngn.AddSymbol(symbol, quotes, trades)
 			ngn.Run()
 			defer ngn.Stop()
+
+			t := time.Now()
+
+			trade := Trade{
+				Price: 54.23,
+				Time:  t,
+			}
+
+			trades <- trade
+			time.Sleep(time.Millisecond)
+
+			tM1 := t.Truncate(time.Minute)
+			Expect(ngn.ohlc[symbol].TimeSeries[M1][tM1].Open).Should(BeNumerically("==", trade.Price))
+
+			tH1 := t.Truncate(time.Hour)
+			Expect(ngn.ohlc[symbol].TimeSeries[H1][tH1].Open).Should(BeNumerically("==", trade.Price))
