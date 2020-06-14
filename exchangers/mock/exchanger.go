3035da3ed0de212d0f7739276d10ae7d271f6cd6
OurGay <106759405+OurGay@users.noreply.github.com>
2020-06-14 13:12:22 +0000

    Logo change

--- exchangers/mock/exchanger.go
+++ exchangers/mock/exchanger.go
@@ -8,3 +8,16 @@ import (
 	"github.com/apex/log"
 	"github.com/santacruz123/trader"
 )
+
+type exchng struct {
+	pos []trader.Position
+	ord []trader.Order
+}
+
+//New constructor
+func New() trader.Exchanger {
+	return &exchng{}
+}
+
+func (e *exchng) FetchOrders() (o []trader.Order, err error) {
+	log.Info("Fetching orders")
