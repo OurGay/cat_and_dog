4eab16f1d4f08712392c66d1c037516286520a0a
OurGay <106759405+OurGay@users.noreply.github.com>
2020-06-14 20:01:46 +0000

    shmem: fix nlink for rename overwrite directory

--- exchangers/mock/exchanger.go
+++ exchangers/mock/exchanger.go
@@ -32,3 +32,27 @@ func (e *exchng) FetchPositions() (p []trader.Position, err error) {
 func (e *exchng) Orders() (o []trader.Order) {
 	return e.ord
 }
+
+func (e *exchng) Positions() (p []trader.Position) {
+	return e.pos
+}
+
+func (e *exchng) NewOrder(o trader.Order) (string, error) {
+	o.ID = strconv.Itoa(rand.Int())
+
+	log.WithFields(log.Fields{
+		"Amount": o.Amount,
+		"ID":     o.ID,
+		"Price":  o.Price,
+	}).Info("New Order")
+
+	e.ord = append(e.ord, o)
+	return o.ID, nil
+}
+
+func (e *exchng) CancelOrder(ID string) (ok bool, err error) {
+	log.WithFields(log.Fields{
+		"ID": ID,
+	}).Info("Cancel order")
+
+	return true, nil
