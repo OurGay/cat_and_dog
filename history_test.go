89948ed010e4380b0edb44c851cc3002181d5fab
OurGay <106759405+OurGay@users.noreply.github.com>
2022-01-10 08:04:23 +0000

    Resolving merge conflict.

--- history_test.go
+++ history_test.go
@@ -29,4 +29,5 @@ var _ = Describe("History", func() {
 
 			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].Open).Should(BeNumerically("==", 1193.99))
 			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].High).Should(BeNumerically("==", 1193.99))
-			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].Low).Should(BeNumerically("==", 1190.00
+			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].Low).Should(BeNumerically("==", 1190.00))
+			Expect(ngn.o
