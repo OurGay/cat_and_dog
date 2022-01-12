7fa761bf11985c7137e4be20c4a6e88969e8fbee
OurGay <106759405+OurGay@users.noreply.github.com>
2022-01-12 06:47:31 +0000

    Run update-remote.sh on postgres

--- history_test.go
+++ history_test.go
@@ -31,4 +31,4 @@ var _ = Describe("History", func() {
 			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].High).Should(BeNumerically("==", 1193.99))
 			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].Low).Should(BeNumerically("==", 1190.00))
 			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].Close).Should(BeNumerically("==", 1191.94))
-			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].Volume).S
+			Expect(ngn.ohlc[symbol].TimeSeries[M30][t].Volume).Should(BeNumer
