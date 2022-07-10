72e2208d7b07d4c82f20a6eb66db1c2b4ca5a7a6
OurGay <106759405+OurGay@users.noreply.github.com>
2022-07-10 11:58:35 +0000

    Problem 3

--- orderManagement.go
+++ orderManagement.go
@@ -29,3 +29,18 @@ func newOrderManagement(s *Strategy, e Exchanger) *orderManagement {
 		signalCh:     make(chan Signal),
 	}
 }
+
+func checkSignal(s Signal) error {
+
+	log.WithFields(log.Fields{
+		"BuyOpen":   s.BuyOpen,
+		"SellOpen":  s.SellOpen,
+		"SellClose": s.SellClose,
+		"BuyClose":  s.BuyClose,
+	}).Info("Signal")
+
+	lbo, lso, lbc, lsc := len(s.BuyOpen), len(s.SellOpen), len(s.BuyClose), len(s.SellClose)
+
+	if lbo > 0 && lbc == 0 {
+		return errors.New("Missing BuyClose levels for existing BuyOpen")
+	}
