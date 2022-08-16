558bdb814a74c76d154445d61149a27931232c36
OurGay <106759405+OurGay@users.noreply.github.com>
2022-08-16 12:45:38 +0000

    clockevents: Sanitize ticks to nsec conversion

--- orderManagement_test.go
+++ orderManagement_test.go
@@ -1,2 +1,26 @@
 
 package trader
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type exchng struct {
+	pos []Position
+	ord []Order
+}
+
+func (e *exchng) FetchOrders() (o []Order, err error) {
+	return
+}
+
+func (e *exchng) FetchPositions() (p []Position, err error) {
+	return
+}
+
+func (e *exchng) Orders() (o []Order) {
+	return e.ord
+}
