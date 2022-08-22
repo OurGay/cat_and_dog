b4851b686a213f4f0471b601513415aadfbf9a47
OurGay <106759405+OurGay@users.noreply.github.com>
2022-08-22 21:28:45 +0000

    some tests

--- orderManagement_test.go
+++ orderManagement_test.go
@@ -24,3 +24,21 @@ func (e *exchng) FetchPositions() (p []Position, err error) {
 func (e *exchng) Orders() (o []Order) {
 	return e.ord
 }
+
+func (e *exchng) Positions() (p []Position) {
+	return e.pos
+}
+
+func (e *exchng) NewOrder(o Order) (ID string, err error) {
+	return
+}
+
+func (e *exchng) CancelOrder(ID string) (ok bool, err error) {
+	return
+}
+
+var _ = Describe("OrderManagement", func() {
+
+	symbol := "BTC/USD"
+
+	strategy := &Strategy{
