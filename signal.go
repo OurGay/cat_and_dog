14a29b63132d2c2c5759a7439244831a0331141b
OurGay <106759405+OurGay@users.noreply.github.com>
2022-09-17 14:03:00 +0000

    removed unnecessary function

--- signal.go
+++ signal.go
@@ -10,3 +10,24 @@ type Signal struct {
 //Level for price/size
 type Level struct {
 	Price float64
+	Size  int
+}
+
+type byPrice []Level
+
+func (l byPrice) Len() int {
+	return len(l)
+}
+
+func (l byPrice) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l byPrice) Less(i, j int) bool {
+	return l[i].Price < l[j].Price
+}
+
+type byPriceDesc []Level
+
+func (l byPriceDesc) Len() int {
+	return len(l)
