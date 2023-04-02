e95496b01e453e8796b9bf044b0dadf183cfe67a
OurGay <106759405+OurGay@users.noreply.github.com>
2023-04-02 16:24:18 +0000

    reverted changes

--- trader_test.go
+++ trader_test.go
@@ -1,2 +1,27 @@
 
 package trader_test
+
+import (
+	"github.com/santacruz123/trader"
+
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Trader", func() {
+
+	Context("Create", func() {
+		It("Simple", func() {
+
+			ma := &trader.Strategy{
+				Title: "Moving average",
+				Code:  "ma",
+				Size:  3,
+			}
+
+			ngn := trader.NewEngine()
+			ngn.AddStrategy(ma)
+			ngn.Run()
+			defer ngn.Stop()
+		})
+	})
+})
